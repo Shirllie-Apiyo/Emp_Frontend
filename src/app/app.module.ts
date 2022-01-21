f6e9301585f97964791b917e40671cdee6a19cd3
Shirllie-Apiyo <apraise320@gmail.com>
2022-01-21 23:54:25 +0300

    chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,25 @@ import { AppComponent } from './app.component';
 import { EmpDetailsComponent } from './emp-details/emp-details.component';
 import { ReadmoreComponent } from './readmore/readmore.component';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 // below needed to run forms
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EmpUpdateComponent } from './emp-update/emp-update.component';
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 
+import { ChartsModule } from 'ng2-charts';
+
 //UI modules
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
@@ -44,6 +46,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     MatSidenavModule,
     MatToolbarModule,
     MatButtonModule,
+    ChartsModule,
     MatMenuModule,
     MatIconModule,
     MatProgressBarModule,
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+
+  lineChartData: ChartDataSets[] = [
+    { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
+  ];
+  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+  lineChartOptions = {
+    responsive: true,
+  };
+  lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,255,0,0.28)',
+    },
+  ];
+  lineChartLegend = true;
+  lineChartPlugins = [];
+  lineChartType: ChartType = "doughnut";  //can change to 'line'
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-  }
+    let API_URL = `http://127.0.0.1:5000/api/countEmployees`
+    this.http.get(API_URL)
+      .subscribe((data) => this.displaydata(data));
+
+    let API_URL2 = `http://127.0.0.1:5000/api/countDepartments`
+    this.http.get(API_URL2)
+      .subscribe((data) => this.displaydata2(data));
+  }// end
+
+  httpdata: any
+  displaydata(data: any) {
+    this.httpdata = data
+    console.log(data);
+  }//this data is exposed to html, httpdata.count
+
+  httpdata2: any
+  displaydata2(data: any) {
+    this.httpdata2 = data
+    console.log(data);
+  }//this data is exposed to html, httpdata.count
+
 
 }
